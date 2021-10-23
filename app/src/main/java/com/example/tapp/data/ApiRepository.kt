f82acf6e489ca91a903dfbcd7f97d26c223d6ff4
Arseniy Prosvirin <arseniy.p@mail.ru>
2021-10-23 12:30:05 +0300

    Added navigation

--- app/src/main/java/com/example/tapp/data/ApiRepository.kt
+++ app/src/main/java/com/example/tapp/data/ApiRepository.kt
@@ -3,8 +3,21 @@ package com.example.tapp.data
 import com.example.tapp.model.User
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+/**
+ * Api events
+ *
+ * @constructor Create empty Api events
+ */
+class ApiEvents
+{
+    var onError : ((code : Int, message : String, exception : Exception?) -> Unit)? = null
+    var onLoadStart : (() -> Unit)? = null
+    var onLoadEnd : (() -> Unit)? = null
 
+}
 class ApiRepository {
+
+    var eventHandler : ApiEvents = ApiEvents()
 
     private var retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(Consts.BASE_URL)
---
+++ app/src/main/java/com/example/tapp/ui/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.tapp.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.tapp.data.ApiRepository
+
+class HomeViewModel(private val apiRepository: ApiRepository) : BaseViewModel(apiRepository) {
+    // TODO: Implement the ViewModel
+}
