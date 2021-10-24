3a9756ebbb9e2207645c8e5dbb2968ade39d0cb7
Arseniy Prosvirin <arseniy.p@mail.ru>
2021-10-24 06:12:12 +0300

    Added booking view

--- app/src/main/java/com/example/tapp/ui/organiser/AccomodationViewFragment.kt
+++ app/src/main/java/com/example/tapp/ui/organiser/AccomodationViewFragment.kt
@@ -1,8 +1,18 @@
 package com.example.tapp.ui.organiser
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.content.res.AssetManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.system.Os.read
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.core.view.children
+import androidx.core.view.isInvisible
 import androidx.fragment.app.findFragment
 import androidx.lifecycle.lifecycleScope
 import com.example.tapp.R
@@ -17,6 +27,8 @@ import com.tomtom.online.sdk.routing.route.description.TravelMode
 import kotlinx.android.synthetic.main.accomodation_view_fragment.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.io.*
+
 
 class AccomodationViewFragment : BaseFragment<AccomodationViewViewModel, AccomodationViewFragmentBinding>(
     AccomodationViewViewModel::class
@@ -58,18 +70,27 @@ class AccomodationViewFragment : BaseFragment<AccomodationViewViewModel, Accomod
                         val routeCalculationDescriptor = RouteCalculationDescriptor.Builder()
                             .routeDescription(routeDescriptor)
                             .build()
-                        val routingApi = OnlineRoutingApi.create(requireContext(), "AwDIXPNqAIMFsVtO3elZXNHQUfEi4qGG")
-                        val spec = RouteSpecification.Builder(LatLng(tomtomMap.userLocation!!.latitude,
-                                                                    tomtomMap.userLocation!!.longitude), location)
+                        val routingApi = OnlineRoutingApi.create(
+                            requireContext(),
+                            "AwDIXPNqAIMFsVtO3elZXNHQUfEi4qGG"
+                        )
+                        val spec = RouteSpecification.Builder(
+                            LatLng(
+                                tomtomMap.userLocation!!.latitude,
+                                tomtomMap.userLocation!!.longitude
+                            ), location
+                        )
                             .routeCalculationDescriptor(routeCalculationDescriptor)
                             .build()
-                        val route = routingApi.planRoute(spec, object : RouteCallback{
+                        val route = routingApi.planRoute(spec, object : RouteCallback {
                             override fun onError(error: RoutingException) {
 
                             }
 
                             override fun onSuccess(routePlan: RoutePlan) {
-                                val routeBuilder = RouteBuilder(routePlan.routes.first().getCoordinates())
+                                val routeBuilder = RouteBuilder(
+                                    routePlan.routes.first().getCoordinates()
+                                )
                                     .style(RouteStyle.DEFAULT_ROUTE_STYLE)
                                 val mapRoute = tomtomMap.addRoute(routeBuilder)
 
@@ -94,7 +115,15 @@ class AccomodationViewFragment : BaseFragment<AccomodationViewViewModel, Accomod
                 }
             }
         }
-
-
+        bookingIV.setOnClickListener {
+            if (pdfv.visibility == View.VISIBLE) {
+                pdfv.visibility = View.GONE
+            } else {
+                pdfv.visibility = View.VISIBLE
+            }
+        }
+        pdfv.fromAsset("booking_com_confirmation.pdf").load();
     }
+
+
 }
